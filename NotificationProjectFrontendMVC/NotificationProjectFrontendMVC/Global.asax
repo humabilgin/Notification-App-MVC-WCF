fe293b49d1d17cfa0dd0327e936fd23e48a9c6df
Hüma Bilgin <46404802+humabilgin@users.noreply.github.com>
2022-06-10 16:27:56 +0300

    Add files via upload

---
+++ NotificationProjectFrontendMVC/NotificationProjectFrontendMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NotificationProjectFrontendMVC.MvcApplication" Language="C#" %>
---
+++ NotificationProjectFrontendMVC/NotificationProjectFrontendMVC/Models/Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotificationProjectFrontendMVC.Models
+{
+
+        public class Home
+        {
+            public int ID;
+            public string Body;
+            public DateTime StartDate;
+            public DateTime EndDate;
+            public bool Status;
+            public short Severity;
+            public DateTime LastUpdateDate; 
+        }
+}
